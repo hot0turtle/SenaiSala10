46136a871c584e2b08475e98bb2123a9abb2b496
hot0turtle <leofarel2015@gmail.com>
2024-02-27 08:14:47 -0300

    fechar consertado e adicionado conteudo da aula perdido

--- activ-03 (restaurante)/PEDIDO/script.js
+++ activ-03 (restaurante)/PEDIDO/script.js
@@ -5,7 +5,7 @@ function comecar(){
 
 function show(nome){
     document.getElementById("nomePrint").textContent = nome;
-    var div = document.getElementById("select-div");
+    var div = document.getElementById("selects-div");
     if (nome && nome.trim() !== ""){
     div.style.display = "block";   
     document.getElementById("exampleFormControlInput1").style.background = "none";
@@ -14,8 +14,8 @@ function show(nome){
         document.getElementById("error").style.display = "flex";
     }
 
+}
     function fechar(){
         document.getElementById("error").style.display = "none"; 
         document.getElementById("exampleFormControlInput1").style.background = "pink";
     }
-}
