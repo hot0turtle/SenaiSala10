48f56a9d3c02da9c48a4a2bf2bcf07237b1f53f4
hot0turtle <leofarel2015@gmail.com>
2024-03-25 09:39:19 -0300

    blob

---
+++ localStorageProject/Carrinho/script.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+    //recupera o carrinho do localstorage ou inicia com um array vazio
+    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+    const listaElement = $('#lista');
+    const totalElement = $('#total');
+    //funcao que vai exibir o carrinho
+    function exibirCarrinho(){
+        listaElement.empty();
+        let totalPreco = 0;
+        $.each(carrinho, function(index, item){
+            //cria uma li
+            const listItem = $("<li>").css('margin-bottom', '5px').text(
+                `${item.descricao} - Preço: R$${item.preco}`
+            )
+            //cria um botao de remover
+            const removeButton = $('<button>').text("❌").css('margin-left', '10px').click(function (){
+                removeItemDoCarrinho(index)
+            });
+           //adicionar o botao a lista
+           listItem.append(removeButton)
+           //adicionar o item a lista
+           listaElement.append(listItem)
+           //adiciona o preco do item no total
+           totalPreco += item.preco;
+        });
+        //exibir o total no elemento totalElement
+        totalElement.text(`Total: R$${totalPreco.toFixed(2)}`);
+    }
+//funcao para remover um item do carrinho
+function removeItemDoCarrinho(index){
+   carrinho.splice(index, 1);
+   localStorage.setItem("carrinho", JSON.stringify(carrinho));
+   exibirCarrinho();
+}
+    exibirCarrinho();
+});
+
+function gerarDocumentoWord(){
+    const listaElement = document.getElementById("lista")
+    const totalElement = document.getElementById("total")
+
+    const listaClone = listaElement.cloneNode(true);
+    
+    $(listaClone).find("button").remove();
+
+    const listaHtml = listaClone.innerHTML;
+    const totalHtml = totalElement.innerHTML;
+    
+    const conteudoHTML = `
+    <html>
+        <head>
+            <meta charset="UTF-8"/>
+        </head>
+        <body>
+            <h1>Pedido confirmado</h1>
+            <h3>Agradecemos a Preferencia</h3>
+            <br>
+            ${listaHtml}
+            <br>
+            <br>
+            ${totalHtml}
+        </body>     
+    </html>
+    `;
+    
+    const blob = new Blob([conteudoHtml], {type: "application/msword"});
+    const link = document.createElement("a");
+
+    link.href = URL.createObjectURL(blob);
+    link.download = "carrinho.doc";
+    link.click();
+    document.getElementById("pedido").style.display = "block";
+
+
+}
+
+const successClose = () => document.getElementById("pedido").style.display = "none";
--- localStorageProject/Login/script.js
+++ localStorageProject/Login/script.js
@@ -1,14 +1,39 @@
 const showPassword = () => {
-    var inputsenha = document.querySelector("#senha");
+    var inputSenha = document.querySelector("#senha");
 
-    if(inputsenha.getAttribute("type") === "password"){
-        inputsenha.setAttribute("type", "text")
+    if(inputSenha.getAttribute("type") === "password"){
+        inputSenha.setAttribute("type", "text")
     }else{
-        inputsenha.setAttribute("type", "password")
+        inputSenha.setAttribute("type", "password")
     }
 }
 
 function login(){
-    var nome = $("nome").val();
-    var senha = $("senha").val();
+    var nome = $("#nome").val();
+    var senha = $("#senha").val();
+
+    if(nome && senha && nome === "admin" && senha === "12345"){
+            const user = {
+                name: nome,
+                dataEntrada: formatarData(new Date()),
+                id: Math.floor(Math.random() * 100000)
+            }
+            localStorage.setItem("usuario", JSON.stringify(user))
+            window.location.href = "../Loja"
+    }else{
+        document.getElementById('error-modal').style.display = "flex"
+    }
+}
+
+const fecharModal = () => document.getElementById('error-modal').style.display = "none"
+
+
+function formatarData(item){
+    var options = {
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+    }
+    return item.toLocaleString("pt-BR", options)
 }
